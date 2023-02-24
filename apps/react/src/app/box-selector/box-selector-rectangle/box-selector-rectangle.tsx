20e09518e2f371703df45d1cb8cf29c36ea7e8a8
alireza-m-sohrabi <alireza.m.sohrabi@gmail.com>
2023-02-24 18:02:18 +0330

    select box

--- apps/react/src/app/box-selector/box-selector-rectangle/box-selector-rectangle.tsx
+++ apps/react/src/app/box-selector/box-selector-rectangle/box-selector-rectangle.tsx
@@ -1,14 +1,85 @@
+import { forwardRef, useRef, useState } from 'react';
+import { createPortal } from 'react-dom';
 import styles from './box-selector-rectangle.module.scss';
 
 /* eslint-disable-next-line */
 export interface BoxSelectorRectangleProps {}
 
-export function BoxSelectorRectangle(props: BoxSelectorRectangleProps) {
-  return (
-    <div onKeyDown={} className={styles['container']}>
-      <h1>Welcome to BoxSelectorRectangle!</h1>
-    </div>
-  );
+export interface BoxSelectorRectangleState {
+  currentPosition?: BoxRectanglePosition;
+  initialPosition?: BoxRectanglePosition;
+  isMoving?: boolean;
 }
+
+export interface BoxRectanglePosition {
+  left: number;
+  top: number;
+}
+
+export interface BoxRectangleState extends BoxRectanglePosition {
+  right: number;
+  bottom: number;
+}
+
+export const BoxSelectorRectangle = forwardRef(
+  (props: BoxSelectorRectangleProps, ref) => {
+    const initialPosition = useRef<BoxRectanglePosition>();
+
+    const [currentPosition, setCurrentPosition] = useState<BoxRectangleState>();
+
+    const [show, setShow] = useState(false);
+
+    function extendRectangle(xCoordinate: number, yCoordinate: number) {
+      if (initialPosition.current) {
+        let bottom = Math.abs(yCoordinate - initialPosition.current.top);
+        let right = Math.abs(xCoordinate - initialPosition.current.left);
+        let top = yCoordinate;
+        let left = xCoordinate;
+
+        if (xCoordinate < initialPosition.current.left) {
+          right = initialPosition.current.left;
+          left = xCoordinate;
+        } else {
+          left = initialPosition.current.left;
+          right = xCoordinate;
+        }
+
+        if (yCoordinate > initialPosition.current.top) {
+          bottom = initialPosition.current.left;
+          top = yCoordinate;
+        } else {
+          top = initialPosition.current.top;
+          right = yCoordinate;
+        }
+
+        setCurrentPosition({
+          bottom,
+          left,
+          top,
+          right,
+        });
+      } else {
+        initialPosition.current = {
+          left: xCoordinate,
+          top: yCoordinate,
+        };
+
+        setShow(true);
+      }
+    }
+
+    function clear() {
+      initialPosition.current = undefined;
+      setShow(false);
+    }
+
+    return createPortal(
+      show ? (
+        <div style={currentPosition} className={styles['container']}></div>
+      ) : null,
+      document.body
+    );
+  }
+);
 
 export default BoxSelectorRectangle;
--- apps/react/src/app/box-selector/box-selector.tsx
+++ apps/react/src/app/box-selector/box-selector.tsx
@@ -1,10 +1,24 @@
+import BoxSelectorRectangle from './box-selector-rectangle/box-selector-rectangle';
 import styles from './box-selector.module.scss';
 
 /* eslint-disable-next-line */
-export interface BoxSelectorProps {
-}
+export interface BoxSelectorProps {}
 
 export function BoxSelector(props: BoxSelectorProps) {
+  function onMouseDown() {
+    onMouseUp();
+    onMouseMove();
+  }
+
+  function onMouseMove() {}
+
+  function onMouseUp() {
+    window.removeEventListener('mousemove', onMouseMove);
+    window.removeEventListener('mouseup', onMouseUp);
+  }
+
+  window.addEventListener('mousedown', onMouseDown);
+
   return (
     <div className={styles['container']}>
       <div className={styles['box-item-container']}>
@@ -12,8 +26,7 @@ export function BoxSelector(props: BoxSelectorProps) {
         <div className={styles['box-item']}></div>
         <div className={styles['box-item']}></div>
       </div>
-
-
+      <BoxSelectorRectangle></BoxSelectorRectangle>
     </div>
   );
 }
